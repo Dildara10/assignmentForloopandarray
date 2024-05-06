6f40decbcc67b174e80790134c86c461a4292dc1
Dildara10 <maha.khan123987@gmail.com>
2024-05-07 00:04:06 +0500

    ForLoop & Array

---
+++ array.js
@@ -0,0 +1,10 @@
+var studentsName = ["dildara", "saima", "naeema",];
+console.log(studentsName);
+studentsName.push("ruqaiya");
+console.log(studentsName);
+studentsName.pop();
+console.log(studentsName);
+studentsName.unshift("ruqaiya");
+console.log(studentsName);
+studentsName.shift();
+console.log(studentsName);
